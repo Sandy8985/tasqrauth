248594a4b412e36e218f6b1a19fd583ca51bcea0
nani67 <pnani18dec@gmail.com>
2023-01-11 10:02:16 +0530

    init with authentication

---
+++ lib/screens/screen71.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Signup extends StatefulWidget {
+  const Signup({super.key});
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios_new),
+        title: Text('Signup'),
+      ),
+    );
+  }
+}
---
+++ lib/screens/screen72.dart
@@ -0,0 +1,60 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tasqrauth/screens/screen71.dart';
+
+FirebaseAuth _auth = FirebaseAuth.instance;
+
+class Signin extends StatefulWidget {
+  const Signin({super.key});
+
+  @override
+  State<Signin> createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  TextEditingController ema = new TextEditingController();
+  TextEditingController pas = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios_new),
+        title: Text('SignIn'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Form(
+            child: Column(
+          children: [
+            Text(
+              'Email Id',
+              textAlign: TextAlign.left,
+            ),
+            TextFormField(
+              controller: ema,
+            ),
+            Text(
+              'Password',
+              textAlign: TextAlign.left,
+            ),
+            TextFormField(
+              controller: pas,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  _auth
+                      .signInWithEmailAndPassword(
+                          email: ema.text, password: pas.text)
+                      .then((x) {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: ((context) => Signup())));
+                  });
+                },
+                child: Text('Submit'))
+          ],
+        )),
+      ),
+    );
+  }
+}
